be73a1c6f9b53473ad03f3c39cce079771983aa2
Prasanna0708 <78546913+Prasanna0708@users.noreply.github.com>
2021-06-14 17:56:54 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,25 @@
+# By using for loop how to print (0,10) values
+#example1:
+for x in range(0,10):
+    print(x)
+#By using for loop print lists values
+#example2:
+print("By using for loop print lists values")
+list_1 = [1,2,3,4,"prasanna","python",10.6,1,11]
+for x in list_1:
+    print(x)
+    
+print("printing tuple values by using for loop")
+tuples_1 = (1,2,3,"prasanna",5,6)
+for x in tuples_1:
+    print(x)
+    
+print("printing sets by using For loop")
+sets_1 = {1,2,3,4,5,6,7,7,8,9,10,10.5}
+for x in sets_1:
+    print(x)
+
+print("printing dictionaries by using for loop")
+dictionary_1 = {1:"one",2:'Two',3:'Three',4:'Four'}
+for x in dictionary_1:
+    print(x)
